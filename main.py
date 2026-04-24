e6300d613d7afdcc1f6c3a6b557ed9b7ac0c6cd4
hhamaad777-a11y <hhamaad777@gmail.com>
2026-04-24 13:41:48 +0300

    Fix main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-l
+import telebot
+
+TOKEN = "8350010692:AAEowYxkV9Pbgi3wqicXQeiXMCGIiBcBG7s"
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands= )
+def send_welcome(message):
+    bot.reply_to(message, "✅ بوت النشر شغال!\nسيبدأ النشر تلقائياً.")
+
+print("✅ البوت شغال بنجاح...")
+bot.infinity_polling()
