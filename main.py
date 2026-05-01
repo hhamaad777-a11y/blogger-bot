5535c7438bba1bc076b92a80b6eb48a444c124cb
hhamaad777-a11y <hhamaad777@gmail.com>
2026-05-01 13:26:49 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import telebot
+import os
 
 TOKEN = "8350010692:AAEowYxkV9Pbgi3wqicXQeiXMCGIiBcBG7s"
 bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands= )
 def send_welcome(message):
-    bot.reply_to(message, "✅ البوت شغال!")
+    bot.reply_to(message, "✅ البوت شغال بنجاح!\nأرسل أي رسالة للتجربة.")
+
+@bot.message_handler(func=lambda message: True)
+def echo(message):
+    bot.reply_to(message, "تم الاستلام: " + message.text)
 
 print("✅ البوت شغال بنجاح...")
 bot.infinity_polling()
